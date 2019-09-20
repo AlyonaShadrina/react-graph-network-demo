477e7e759f050e838a8fe55760ae3b1ad6c6046a
Alyona Shadrina <alyon.shadrina@gmail.com>
2019-09-20 16:42:28 +0300

    [upd] build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9c45746967809c810a14396e35409744.js"
+  "/precache-manifest.4ee67e5b30f9df6a617fb367465f4c4d.js"
 );
 
 self.addEventListener('message', (event) => {
