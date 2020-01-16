f60b8f14bc25f20fd9d4953c3f3d7721055ceaa5
Alyona Shadrina <alyon.shadrina@gmail.com>
2020-01-16 12:54:57 +0200

    Updates

--- precache-manifest.a832f6a24650174b2bb32d651bc92986.js
+++ precache-manifest.a832f6a24650174b2bb32d651bc92986.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c430cbb5773ff1c7df1c0bf7c260d177",
+    "revision": "696bbff492ac6af6171c4a98b74a1c6f",
     "url": "/react-graph-network-demo/index.html"
   },
   {
-    "revision": "e4c637f75bae389147d9",
+    "revision": "34a6f6feeacd1100515b",
     "url": "/react-graph-network-demo/static/css/main.76917063.chunk.css"
   },
   {
-    "revision": "da30437cd89ccf3f27d0",
-    "url": "/react-graph-network-demo/static/js/2.34207e18.chunk.js"
+    "revision": "534f977b897181d04441",
+    "url": "/react-graph-network-demo/static/js/2.836bb8dc.chunk.js"
   },
   {
     "revision": "3a2edbac0b625403cc8cad9dc7e17c15",
-    "url": "/react-graph-network-demo/static/js/2.34207e18.chunk.js.LICENSE"
+    "url": "/react-graph-network-demo/static/js/2.836bb8dc.chunk.js.LICENSE"
   },
   {
-    "revision": "e4c637f75bae389147d9",
-    "url": "/react-graph-network-demo/static/js/main.b65ba792.chunk.js"
+    "revision": "34a6f6feeacd1100515b",
+    "url": "/react-graph-network-demo/static/js/main.61fb2320.chunk.js"
   },
   {
     "revision": "3aa0417031b4e4c1a74e",
